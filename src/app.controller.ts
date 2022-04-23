dd2ca4ba697306556c3d5b19c59d99fec83a5101
Nasa Sutisna <nasa.sutisna@greatdayhr.com>
2022-04-23 10:31:48 +0700

    update get hello

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
+
+  @Get('/')
+  getHello(): any {
+    return this.appService.getHello();
+  }
 
   @Post('/generate')
   generateLink(@Body() body: any): any {
