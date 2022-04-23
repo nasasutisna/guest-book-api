37ec752e1170b2d706bc7518d4f583af0715edbd
Nasa Sutisna <nasa.sutisna@greatdayhr.com>
2022-04-23 10:50:30 +0700

    add response error

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { ResponseData } from './dto/app.dto';
+import { responseData, responseError } from './dto/app.dto';
 import { createFile, getFile } from './helpers/storage.helper';
 
 @Injectable()
@@ -8,11 +8,11 @@ export class AppService {
   private readonly apiWA = 'https://api.whatsapp.com/send?phone=';
 
   getHello() {
-    return new ResponseData('hello word', 'success');
+    return responseData('hello word', 'success');
   }
 
   getHello2() {
-    return new ResponseData('hello word 2', 'success');
+    return responseData('hello word 2', 'success');
   }
 
   async generateLink(path = 'list-nama.txt', templateMessage = 'template.txt') {
@@ -42,7 +42,7 @@ export class AppService {
 
       const value = templateReplace.join('\n\n');
       await createFile('output', 'result-link.txt', value);
-      return new ResponseData(value, 'success');
+      return responseData(value, 'success');
     } catch (error) {
       console.log(error);
     }
@@ -50,41 +50,39 @@ export class AppService {
 
   async generatLinkWA(data = [], templateMessage = 'template.txt') {
     try {
-      return new ResponseData(data, 'success');
-      // if (!data?.length) {
-      //   return new ResponseData(null, 'success');
-      // }
+      if (!data?.length) {
+        return responseData(null, 'success');
+      }
 
-      // const names = data.map((item) => item?.name);
-      // const ids = data.map((item) => item?.id);
-      // const noHP = data.map((item) => item?.phone);
-      // const linkInvitation = data.map((item) => {
-      //   const convertLink = `${this.link}${item?.name.split(' ').join('+')}`;
-      //   return convertLink;
-      // });
+      const names = data.map((item) => item?.name);
+      const ids = data.map((item) => item?.id);
+      const noHP = data.map((item) => item?.phone);
+      const linkInvitation = data.map((item) => {
+        const convertLink = `${this.link}${item?.name.split(' ').join('+')}`;
+        return convertLink;
+      });
 
-      // const getTemplate = await getFile(templateMessage);
-      // const result = [];
-      // let index = 0;
-      // for (const item of names) {
-      //   let newTemplate = getTemplate.toString();
-      //   newTemplate = newTemplate.replace('{{name}}', item);
-      //   newTemplate = newTemplate.replace('{{link}}', linkInvitation[index]);
-      //   const linkWA = `${this.apiWA}${noHP[index]}&text=${encodeURIComponent(newTemplate)}`;
-      //   result.push({
-      //     id: ids[index],
-      //     name: item,
-      //     phone: noHP[index],
-      //     linkWA,
-      //     content: encodeURIComponent(newTemplate)
-      //   });
-      //   index++;
-      // }
+      const getTemplate = await getFile(templateMessage);
+      const result = [];
+      let index = 0;
+      for (const item of names) {
+        let newTemplate = getTemplate.toString();
+        newTemplate = newTemplate.replace('{{name}}', item);
+        newTemplate = newTemplate.replace('{{link}}', linkInvitation[index]);
+        const linkWA = `${this.apiWA}${noHP[index]}&text=${encodeURIComponent(newTemplate)}`;
+        result.push({
+          id: ids[index],
+          name: item,
+          phone: noHP[index],
+          linkWA,
+          content: encodeURIComponent(newTemplate)
+        });
+        index++;
+      }
 
-      // return new ResponseData(result, 'success');
+      return responseData(result, 'success');
     } catch (error) {
-      throw new Error(error)
-      console.log(error);
+      return responseError(error);
     }
   }
 }
--- src/dto/app.dto.ts
+++ src/dto/app.dto.ts
@@ -1,3 +1,5 @@
+import { HttpException } from "@nestjs/common";
+
 export class ResponseData {
   public data: any;
   public message: any;
@@ -6,3 +8,7 @@ export class ResponseData {
     this.message = message;
   }
 }
+export const responseData = (data, message) => new ResponseData(data, message);
+export const responseError = (message: string, code = 400) => {
+  return Promise.reject(new HttpException({ message }, code));
+};
